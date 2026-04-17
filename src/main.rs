76afa4778189a41bed4173dd4ee89d01ce9afe4b
Richard Alves <git@richardalves.com>
2026-04-17 19:19:14 -0300

    feat: extrai titulo e quantidade de videos da playlist

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,34 @@
 use serde::{Deserialize, Serialize};
 use std::io::{self, Write};
 
+use serde_json::Value;
+
 
 fn main() {
     let api_token = dotenvy::var("YTB_API_TOKEN").expect("A variável de ambiente `YTB_API_TOKEN` não foi encontrada.");
 
+    //ask_for_playlist_id("Insira o ID da Playlist: ");
+    let playlist_id = "PLHg022HMFzFCRq-5ZVR3hiiCkGPJ3Ur1D"; // hardcoded por enquanto para facilitar
 
+    // Passo 1: request para /playlist para obter nome da playlist e quantidade de vídeos
+    let response = reqwest::blocking::get(format!("https://www.googleapis.com/youtube/v3/playlists?part=contentDetails,snippet&id={playlist_id}&key={api_token}")).expect("Erro no request 1");
+
+    let body = response.text().expect("Erro ao ler body do primeiro request");
+
+    let json: Value = serde_json::from_str(&body).expect("Erro ao transformar body em json");
+        
+    let playlist_title = json["items"][0]["snippet"]["title"].as_str().expect("Erro ao obter título da playlist");
+    
+    let videos_count = json["items"][0]["contentDetails"]["itemCount"].as_u64().expect("Não foi possível verificar a quantidade de vídeos na playlist");
+
+    println!("A playlist {playlist_title} tem {videos_count} videos."); // apenas para debug
+
+    // Passo 2: request (ou loop) para /playlistItems para obter o id de cada vídeo
+
+    // Passo 3: request para /videos para obter titulo e duração de cada vídeo
 }
 
-fn ask_for_playlist_id(msg: &str) -> String {
+fn _ask_for_playlist_id(msg: &str) -> String {
 
     let mut buf = String::new();
     print!("{msg}");
